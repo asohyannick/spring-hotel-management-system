3ca665cab89d0a6669cff87b1a066025ed13266b
asohyannick <asoh@cskytelecoms.com>
2025-12-06 14:14:08 +0100

    I've completed the foundational structure of my hotel management system with Spring Boot and PostgreSQL configuration successfully and we're are now ready to start building our API endpoints

---
+++ src/main/java/com/hotelCare/hostelCare/HostelCareApplication.java
@@ -0,0 +1,9 @@
+package com.hotelCare.hostelCare;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HostelCareApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HostelCareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hotelCare/hostelCare/HostelCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelCare.hostelCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
